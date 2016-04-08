8ae34c44994f9168911e6ab1309851fdef1bf3a1
NicolasMoan <nic_hell@hotmail.com>
2016-04-08 15:03:23 +0200

    Ajout des followers

--- src/main/webapp/app/entities/tweet/tweet.controller.js
+++ src/main/webapp/app/entities/tweet/tweet.controller.js
@@ -5,26 +5,39 @@
         .module('tweeterApp')
         .controller('TweetController', TweetController);
 
-    TweetController.$inject = ['$scope', '$state', 'Tweet', 'User', 'Principal', 'LoginService'];
+    TweetController.$inject = ['$scope', '$state','$stateParams', 'Tweet', 'User', 'Principal', 'LoginService'];
 
-    function TweetController ($scope, $state, Tweet, User, Principal, LoginService) {
+    function TweetController ($scope, $state,$stateParams, Tweet, User, Principal, LoginService) {
         var vm = this;
         vm.alltweets = [];
         vm.tweets = [];
-
+        vm.user = {};
+        vm.followers = [];
         
-        vm.filte = function() {
-        	
-        };
+        
         vm.loadAll = function() {
+
+            
             Tweet.query(function(result) {
                 vm.alltweets = result;
-                
-                for (var i = 0; i < vm.alltweets.length; i++) { 
-            	    if (vm.alltweets[i].user.login == vm.account.login) {
-            	    	vm.tweets.push(vm.alltweets[i]);
-            	    };
-            	};
+                User.get({login: vm.account.login}, function(result) {
+                    vm.user = result;
+                    vm.followers = vm.user.followers;
+                    
+                    for (var i = 0; i < vm.alltweets.length; i++) { 
+                	    if (vm.alltweets[i].user.login == vm.account.login) {
+                	    	vm.tweets.push(vm.alltweets[i]);
+                	    } else {
+                	    	
+                	    	for (var i = 0; i < vm.followers.length; i++) { 
+                	    		if (vm.followers[i].login == vm.alltweets[i].user.login) {
+                	    			vm.tweets.push(vm.alltweets[i]);
+                	    		}
+                	    	}
+                	    }
+                	};
+                    
+                });
             });
             
             
@@ -46,5 +59,7 @@
 
         getAccount();
         vm.loadAll();
+        
+        
     }
 })();
